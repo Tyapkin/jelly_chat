3d231f9f732aec5fd12e9d6c7f19776d97d5733d
Alexander Tyapkin <atyapkin20@gmail.com>
2019-03-24 21:51:42 +0200

    vue frontend created

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import JellyChat from '@/components/JellyChat'
+import AuthenticationForm from '@/components/AuthenticationForm'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+const router = new Router({
+    routes: [
+        {
+            path: '/chat',
+            name: 'JellyChat',
+            component: JellyChat
+        },
+        {
+            path: '/auth',
+            name: 'AuthenticationForm',
+            component: AuthenticationForm
+        }
+    ]
 })
+
+router.beforeEach((to, from, next) => {
+    if (sessionStorage.getItem('authToken') !== null || to.path === '/auth') {
+        next()
+    } else {
+        next('/auth')
+    }
+})
+
+export default router
